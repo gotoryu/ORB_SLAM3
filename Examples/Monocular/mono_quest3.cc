edf54372116f66cd9cee3c1748b8f43b449f43e3
gotoryu <badialbahra@gmail.com>
2026-08-21 17:56:18 +0200

    monocular tracking with quest 3 seems to be working now

--- Examples/Monocular/mono_quest3.cc
+++ Examples/Monocular/mono_quest3.cc
@@ -15,14 +15,14 @@
 
 using namespace std;
 
-void LoadImages(const string &path, vector<string> &vstrImage, vector<double> &vTimeStamps);
+void LoadImages(const string &path, vector<string> &vstrImage, vector<double> &vTimeStamps, int fps = 20);
 
 int main(int argc, char **argv)
 {
      if (argc < 5)
      {
           cerr << endl
-               << "Usage: ./mono_s20fe path_to_vocabulary path_to_settings fps path_to_image_folder_1 (path_to_image_folder_2 ... path_to_image_folder_n)" << endl;
+               << "Usage: ./mono_quest3 path_to_vocabulary path_to_settings fps path_to_image_folder_1 (path_to_image_folder_2 ... path_to_image_folder_n)" << endl;
           return 1;
      }
 
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
           string pathSeq(argv[seq + 4]);
           string pathCam0 = pathSeq + "/cam0";
 
-          LoadImages(pathCam0, vstrImages[seq], vTimestamps[seq]);
+          LoadImages(pathCam0, vstrImages[seq], vTimestamps[seq], fps);
           cout << "LOADED!" << endl;
 
           nImages[seq] = vstrImages[seq].size();
@@ -60,8 +60,9 @@ int main(int argc, char **argv)
      vector<float> vTimesTrack;
      vTimesTrack.resize(tot_images);
      cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(3.0, cv::Size(8, 8));
-
      cv::Mat im;
+     int global_image_index = 0;
+     
      for (seq = 0; seq < num_seq; seq++)
      {
           cout << endl
@@ -99,7 +100,7 @@ int main(int argc, char **argv)
                std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
 
                double ttrack = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count();
-               vTimesTrack[ni * (seq + 1)] = ttrack;
+               vTimesTrack[global_image_index++] = ttrack;
 
                double T = 0;
                if (ni < nImages[seq] - 1)
@@ -129,7 +130,7 @@ int main(int argc, char **argv)
 
      SLAM.Shutdown();
 
-     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory_S20FE.txt");
+     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory_Quest3.txt");
 
      sort(vTimesTrack.begin(), vTimesTrack.end());
      float totaltime = 0;
@@ -148,27 +149,63 @@ int main(int argc, char **argv)
      return 0;
 }
 
-void LoadImages(const string &path, vector<string> &vstrImage, vector<double> &vTimeStamps)
+void LoadImages(const string &path, vector<string> &vstrImage, vector<double> &vTimeStamps, int fps)
 {
      vector<cv::String> imagePaths;
      cv::glob(path + "/*.png", imagePaths, false);
+     int index = 1;
 
      // Sort to ensure chronological order
      std::sort(imagePaths.begin(), imagePaths.end());
 
-     for (size_t i = 0; i < imagePaths.size(); i++)
+     stringstream fst;
+     fst << setfill('0') << setw(5) << index;
+     string firstFile = path + "/" + fst.str() + ".png";
+     ifstream ff(firstFile.c_str());
+
+     if (ff.good())
      {
-          string leftPath = imagePaths[i];
+          double frame_time = 1.0 / fps;
+          std::cout << "Image name format: 12345.png (framerate is fixed at " << fps << " FPS)" << std::endl;
 
-          // Extract filename without path and extension
-          size_t lastSlash = leftPath.find_last_of("/\\");
-          size_t lastDot = leftPath.find_last_of(".");
-          string filename = leftPath.substr(lastSlash + 1, lastDot - lastSlash - 1);
+          while (true)
+          {
+               stringstream ss;
+               ss << setfill('0') << setw(5) << index;
+               string filename = path + "/" + ss.str() + ".png";
 
-          vstrImage.push_back(leftPath);
+               ifstream f(filename.c_str());
+               if (!f.good())
+               {
+                    break;
+               }
 
-          // Convert filename (nanoseconds) to seconds
-          double t = std::stod(filename) / 1e9;
-          vTimeStamps.push_back(t);
+               vstrImage.push_back(filename);
+
+               double current_time = (index - 1) * frame_time;
+               vTimeStamps.push_back(current_time);
+
+               index++;
+          }
+     }
+     else
+     {
+          std::cout << "Image name format: 1234567891011121314.png (name represents timestamp)" << std::endl;
+
+          for (size_t i = 0; i < imagePaths.size(); i++)
+          {
+               string leftPath = imagePaths[i];
+
+               // Extract filename without path and extension
+               size_t lastSlash = leftPath.find_last_of("/\\");
+               size_t lastDot = leftPath.find_last_of(".");
+               string filename = leftPath.substr(lastSlash + 1, lastDot - lastSlash - 1);
+
+               vstrImage.push_back(leftPath);
+
+               // Convert filename (nanoseconds) to seconds
+               double t = std::stod(filename) / 1e9;
+               vTimeStamps.push_back(t);
+          }
      }
 }
